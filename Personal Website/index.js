22d0ce845359dfd44324ee4c774b07930a7fbaff
marty <mart.yordanov@gmail.com>
2023-03-08 10:14:35 +0200

    button function optimisation

--- Personal Website/index.js
+++ Personal Website/index.js
@@ -1,81 +1,39 @@
-
-let experiment=document.getElementById('aboutMartin');
-let experimental=document.getElementById("about-me");
-function unhideList(event){
-    if(experiment.style.visibility==='hidden'){
-        experiment.style.visibility='visible'
+function unhideList(domElement){
+    if(domElement.style.visibility==='hidden'){
+        domElement.style.visibility='visible'
     }else{
-        experiment.style.visibility='hidden'
+        domElement.style.visibility='hidden'
     }   
-}
-
-experimental.addEventListener('click',unhideList)
+};
 
 
-
-let experiment1=document.getElementById('certificates');
-let experimental1=document.getElementById("education");
-function unhideList1(event){
-    if(experiment1.style.visibility==='hidden'){
-        experiment1.style.visibility='visible'
-    }else{
-        experiment1.style.visibility='hidden'
-    }
-}
-experimental1.addEventListener('click',unhideList1)
+let dropDown=document.getElementById('aboutMartin');
+let aboutMeButton=document.getElementById("about-me");
+aboutMeButton.addEventListener('click', unhideList.bind(null, dropDown));
 
 
 
-let experiment2=document.getElementById('worked');
-let experimental2=document.getElementById("experience");
-function unhideList2(event){
-    if(experiment2.style.visibility==='hidden'){
-        experiment2.style.visibility='visible'
-    }else{
-        experiment2.style.visibility='hidden'
-    }
-}
-experimental2.addEventListener('click',unhideList2)
+let dropDown1=document.getElementById('certificates');
+let educationButton=document.getElementById("education");
+educationButton.addEventListener('click',unhideList.bind(null,dropDown1));
 
 
 
-let experiment3=document.getElementById('proglanguages');
-let experimental3=document.getElementById("skills");
-function unhideList3(event){
-    if(experiment3.style.visibility==='hidden'){
-        experiment3.style.visibility='visible'
-    }else{
-        experiment3.style.visibility='hidden'
-    }
-}
-experimental3.addEventListener('click',unhideList3)
+let dropDown2=document.getElementById('worked');
+let experienceButton=document.getElementById("experience");
+experienceButton.addEventListener('click',unhideList.bind(null,dropDown2))
 
 
 
-
-let experiment4=document.getElementById('form');
-let experimental4=document.getElementById("taste");
-function unhideList4(event){
-    if(experiment4.style.visibility==='hidden'){
-        experiment4.style.visibility='visible'
-    }else{
-        experiment4.style.visibility='hidden'
-    }
-}
-experimental4.addEventListener('click',unhideList4)
+let dropDown3=document.getElementById('proglanguages');
+let skillsButton=document.getElementById("skills");
+skillsButton.addEventListener('click',unhideList.bind(null,dropDown3))
 
 
 
-
-
-
-
-
-
-
-
-
-
+let dropDown4=document.getElementById('form');
+let contactFormButton=document.getElementById("contactForm");
+contactFormButton.addEventListener('click',unhideList.bind(null,dropDown4))
 
 
 
